a439f8d58933758b8b10b71b8f38d5d491321532
Sherif Nasr <shrief.nasr@gmail.com>
2022-01-30 14:42:29 +0200

    gghghghgh

--- LatestNews/LatestNews/Base/Extensions/UIViewExtension.swift
+++ LatestNews/LatestNews/Base/Extensions/UIViewExtension.swift
@@ -9,7 +9,7 @@ import UIKit
 
 extension UIView {
     /**
-     adding shadow to view 
+     new kalam
      */
     func addShadow(){
         layer.shadowColor = .init(red: 0, green: 0, blue: 0, alpha: 16)
